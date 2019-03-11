925baae26042a0f6af52ff10cf7f49cb90b4a534
kamal <kamal>
2019-03-11 17:30:53 +0800

    注音符号文档

--- src/assets/scripts/FuriganaMD.js
+++ src/assets/scripts/FuriganaMD.js
@@ -1,3 +1,7 @@
+// 注音功能来自于
+// https://github.com/amclees/furigana-markdown
+// 详见上述文档
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
